942d258b3b51568dc881b6d4ac06da37588465d2
LE Palmer <lpalmer@ucsd.edu>
2023-10-30 22:16:43 -0700

    Created questions array array to store quiz  questions, answers and correct answer

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -25,4 +25,56 @@ function startQuiz() {
         btnList.append(btn)
     }
 }
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create array to store questions, answers and correct answer
+var theQuestions = [
+    {
+        question: "Arrays in JavaScript are defined by which of the following statements?",
+        answers: ["It is an ordered list of values", "It is an ordered list of objects", "It is an ordered list of strings", "It is an ordered list of functions"],
+        correct: "It is an ordered list of values"
+    }, {
+        question: "Which of the following is not a Javascript data type?",
+        answers: ["Null", "Array", "Boolean", "Function"],
+        correct: "Function"
+    }, {
+        question: "Which of the following can be used to call a JavaScript Code Snippet?",
+        answers: ["Function/Method", "Preprocessor", "Triggering Event", "Condition"],
+        correct: "Function/Method"
+    }, {
+        question: "Which method attaches an event handler to the specified element?",
+        answers: ["getElementById", "innerHTML", "addEventListener", "none of the above"],
+        correct: "addEventListener"
+    }, {
+        question: "Which function is used to convert an object into a string?",
+        answers: ["parse()", "stringify()", "convert()", "none of the above"],
+        correct: "stringify()"
+    }
+]
